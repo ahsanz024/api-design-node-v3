430666f9e57c27111590622e177bbeeef5293e4c
Ahsan <ahsan024@gmail.com>
2020-06-26 12:59:22 +0200

    add: basic get/post routes

--- src/server.js
+++ src/server.js
@@ -7,9 +7,27 @@ export const app = express()
 
 app.disable('x-powered-by')
 
+// middlewares
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World!'
+  })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({
+    message: 'ok'
+  })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server is running')
+  })
+}
