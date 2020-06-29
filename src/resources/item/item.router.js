a7f10400af96c5cc301b25bb5c1bcff0f648c8d1
Ahsan <ahsan024@gmail.com>
2020-06-29 13:00:25 +0200

    add: itemRouter `api/item`

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
 import { Router } from 'express'
 
+const mockController = (req, res) => {
+  res.json({ message: 'Mock Controller from Item router' })
+}
+
 const router = Router()
+
+// /api/item
+router
+  .route('/')
+  .get(mockController)
+  .post(mockController)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get(mockController)
+  .put(mockController)
+  .delete(mockController)
 
 export default router
